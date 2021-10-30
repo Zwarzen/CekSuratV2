d1975e9f415f93778b46fa2d0a8e99859bb09a76
Risang <akurisang12@gmail.com>
2021-10-30 08:17:08 +0700

    add java.Menu and Layout

---
+++ app/src/main/java/org/aplas/ceksuratv2/Menu.java
@@ -0,0 +1,22 @@
+package org.aplas.ceksuratv2;
+
+public class Menu {
+    private String name;
+    private int photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+}
